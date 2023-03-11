dcb9118ed3bb778a9125e41eacf231ae74fb9995
Nicolas Mdlb <nicolas.mdlb@gmail.com>
2023-03-11 19:01:39 +0100

    Exo numbers in array

---
+++ Array/array.js
@@ -0,0 +1,26 @@
+const numbers = [1, 4, 3, 7, 6, 3, 7]
+console.log(numbers)
+
+// Classez le tablaeau dans l'ordre (sort) du plus petit au plus grand
+
+console.log('Number Ordering : ', 'your new array')
+
+// Enlevez les chiffres en double 
+
+console.log('Number double', )
+
+// Ajoutez les nombres manquants via une function
+
+console.log('Add number missing', 'your new array')
+
+/// Ceci est une documentation
+/**
+ * Add numbers missing in array
+ * @param {Array<number>} listNumbers 
+ * @returns 
+ */
+function AddMissingNumberInArray(listNumbers){
+    let newListNumbers = []
+
+    return newListNumbers
+}
